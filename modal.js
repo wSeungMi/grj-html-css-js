74114bfdc15de53db2022dd616310c31a2417645
SEUNGMI <seungmi.dev@gmail.com>
2022-05-22 23:07:08 +0900

    Modal + AddItems

--- modal.js
+++ modal.js
@@ -1,25 +1,30 @@
 'use strict';
 
 const modal = document.getElementById("modal");
-const addItembtn = document.getElementById("addItem");
+const openModal = document.getElementById("addItem");
 const closeModal = document.getElementById("closeModal");
 
 // 1. modal창 숨겨두기
 modal.style.display = "none";
 
 // 2. modal창 보이기
-addItembtn.addEventListener('click', () => {
+openModal.addEventListener('click', () => {
     modal.style.display = "block";
 });
 
 // 3. X버튼으로 modal창 닫기
 closeModal.addEventListener('click', () => {
+    document.getElementById("modalName").value = "";
+    document.getElementById("modalImg").value = "";
+    document.getElementById("modalContent").value = "";
     modal.style.display = "none";
 });
 
 
 // --------- 4~7은 addItem.js ---------
 
-
+function addCloseModal() {
+    document.getElementById("modal").style.display = "none";
+}
 
 
